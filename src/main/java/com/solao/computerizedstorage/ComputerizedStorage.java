3033e0f9a15bdee2bb09072aca917dfa6e9cfea5
Sean Stoves <sean@seanstoves.com>
2024-09-30 23:53:29 -0400

    Add database configuration and connection setup
    Refactored `ItemStorage` to support H2 and MariaDB backends based on the configuration. Updated `ComputerizedStorage` to save default config files on enable and load configurations.

--- src/main/java/com/solao/computerizedstorage/ComputerizedStorage.java
+++ src/main/java/com/solao/computerizedstorage/ComputerizedStorage.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ComputerizedStorage extends JavaPlugin {
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        loadConfig();
         this.getCommand("openstorage").setExecutor(new StorageCommand());
         getServer().getPluginManager().registerEvents(new StorageGUI(), this);
         getLogger().info("ComputerizedStorage has been enabled!");
@@ -14,5 +16,10 @@ public class ComputerizedStorage extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("ComputerizedStorage has been disabled!");
+    }
+
+    private void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
     }
 }
--- src/main/java/com/solao/computerizedstorage/ItemStorage.java
+++ src/main/java/com/solao/computerizedstorage/ItemStorage.java
@@ -1,16 +1,38 @@
-// File: src/main/java/com/solao/computerizedstorage/ItemStorage.java
 package com.solao.computerizedstorage;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.inventory.ItemStack;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemStorage {
     private final Map<String, ItemStack> storedItems;
+    private final Connection dbConnection;
 
-    public ItemStorage() {
+    public ItemStorage(JavaPlugin plugin) throws SQLException {
         this.storedItems = new HashMap<>();
+        FileConfiguration config = plugin.getConfig();
+        String dbType = config.getString("storage.backend", "h2");
+
+        String dbUrl;
+        if ("mariadb".equalsIgnoreCase(dbType)) {
+            String host = config.getString("storage.mariadb.host", "localhost");
+            int port = config.getInt("storage.mariadb.port", 3306);
+            String database = config.getString("storage.mariadb.database", "minecraft");
+            String user = config.getString("storage.mariadb.user", "root");
+            String password = config.getString("storage.mariadb.password", "password");
+            dbUrl = "jdbc:mariadb://" + host + ":" + port + "/" + database;
+            dbConnection = DriverManager.getConnection(dbUrl, user, password);
+        } else {
+            String dbPath = config.getString("storage.h2.path", "database");
+            dbUrl = "jdbc:h2:" + dbPath;
+            dbConnection = DriverManager.getConnection(dbUrl);
+        }
     }
 
     public void storeItem(String id, ItemStack item) {
@@ -23,5 +45,9 @@ public class ItemStorage {
 
     public Map<String, ItemStack> getStoredItems() {
         return storedItems;
+    }
+
+    public Connection getDbConnection() {
+        return dbConnection;
     }
 }
